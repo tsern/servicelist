7bce45d3178b23857881e4870ffec55de9378258
Tania <tania.sernivka@gmail.com>
2019-01-25 09:01:18 +0200

    added create and index routes to company container

---
+++ app/Containers/Company/UI/API/Routes/create.v1.private.php
@@ -0,0 +1,29 @@
+<?php
+//
+///**
+// * @apiGroup           Company
+// * @apiName            CreateCompanyAction
+// *
+// * @api                {POST} /v1/create Endpoint title here..
+// * @apiDescription     Endpoint description here..
+// *
+// * @apiVersion         1.0.0
+// * @apiPermission      none
+// *
+// * @apiParam           {String}  parameters here..
+// *
+// * @apiSuccessExample  {json}  Success-Response:
+// * HTTP/1.1 200 OK
+//{
+//  // Insert the response of the request here...
+//}
+// */
+//
+///** @var Route $router */
+//$router->post('create', [
+//    'as' => 'api_company_create_company_action',
+//    'uses'  => 'Controller@CreateCompanyAction',
+//    'middleware' => [
+//      'auth:api',
+//    ],
+//]);
---
+++ app/Containers/Company/UI/API/Routes/index.v1.private.php
@@ -0,0 +1,29 @@
+<?php
+//
+///**
+// * @apiGroup           Company
+// * @apiName            GetAllCompanies
+// *
+// * @api                {GET} /v1/index Endpoint title here..
+// * @apiDescription     Endpoint description here..
+// *
+// * @apiVersion         1.0.0
+// * @apiPermission      none
+// *
+// * @apiParam           {String}  parameters here..
+// *
+// * @apiSuccessExample  {json}  Success-Response:
+// * HTTP/1.1 200 OK
+//{
+//  // Insert the response of the request here...
+//}
+// */
+//
+///** @var Route $router */
+//$router->get('index', [
+//    'as' => 'api_company_get_all_companies',
+//    'uses'  => 'Controller@GetAllCompanies',
+//    'middleware' => [
+//      'auth:api',
+//    ],
+//]);
