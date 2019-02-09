2227dfccc5f8b84dafa8eb24e8be89b266759855
Tania <tania.sernivka@gmail.com>
2019-02-09 19:14:51 +0200

    cosmetic changes in view pages

--- app/Containers/Company/UI/WEB/Views/show_edit_page.blade.php
+++ app/Containers/Company/UI/WEB/Views/show_edit_page.blade.php
@@ -1,40 +1,43 @@
-<div>
-    <div>
-        <div>
-            <h3>Update Company</h3>
-        </div>
-        <div>
-            <div>
-                <form method="POST" action="{{ url('company/update', ['id' => $company->id]) }}"  role="form" enctype="multipart/form-data">
-                    {{ csrf_field() }}
-                    <input name="_method" type="hidden" value="PATCH">
-                    <div>
-                        <div>
-                            <div>
-                                <input type="text" name="name" value="{{$company->name}}" id="name" placeholder="Company name">
+@extends('layouts.one_column_template')
+
+@section('content')
+<div class="col-lg-8">
+    <div class="card">
+        <form method="POST" action="{{ url('company/update', ['id' => $company->id]) }}"  role="form" enctype="multipart/form-data">
+            {{ csrf_field() }}
+            <div class="card-body">
+                <h3 class="card-title">Update Company</h3>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <input name="_method" type="hidden" value="PATCH">
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="name" value="{{$company->name}}" id="name" placeholder="Company name">
                             </div>
                         </div>
                     </div>
 
-                    <div>
-                        <textarea name="description" class="form-control input-sm" placeholder="Description">{{$company->description}}</textarea>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <textarea rows="5" class="form-control" name="description" placeholder="Description">{{$company->description}}</textarea>
+                        </div>
                     </div>
-                    <div>
-                        <div>
-                            <div>
-                                <input type="file" name="logo" required>
+
+                    <div class="col-md-12">
+                        <div class="form-group">
+                                <input type="file" class="form-control" name="logo">
                             </div>
                         </div>
                     </div>
 
-                    <div>
-                        <div>
-                            <input type="submit"  value="Update">
-                            <a href="{{ url('company/index') }}">Back</a>
-                        </div>
+                <div class="form-footer">
+                    <div class="btn-list">
+                        <input type="submit" class="btn btn-success btn-block" value="Update">
+                        <a href="{{ url('company/index') }}" class="btn btn-danger btn-block">Back</a>
                     </div>
-                </form>
+                </div>
             </div>
-        </div>
+        </form>
     </div>
 </div>
+@endsection
